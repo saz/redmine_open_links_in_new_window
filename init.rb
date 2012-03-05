7a37d8edc1f02d8411b1fd7db88c45b676571b59
Steffen Zieger <me@saz.sh>
2012-03-05 14:08:39 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,11 +3,11 @@ require 'redmine'
 require_dependency 'redmine_open_links_in_new_window/view_layouts_base_html_head_hook'
 
 Redmine::Plugin.register :redmine_open_links_in_new_window do
-  name 'Redmine Open Links In New Window plugin'
-  author 'akzhan.abdulin@gmail.com'
+  name 'Redmine Open Links In New Window plugin (except Wiki URLs)'
+  author 'me@saz.sh'
   description 'This is the Open Links In New Window plugin for Redmine'
-  version '0.0.2'
-  url 'http://github.com/Undev/redmine_open_links_in_new_window'
-  author_url 'http://github.com/Undev'
+  version '0.0.3'
+  url 'https://github.com/saz/redmine_open_links_in_new_window'
+  author_url 'http://github.com/saz'
 end
 
